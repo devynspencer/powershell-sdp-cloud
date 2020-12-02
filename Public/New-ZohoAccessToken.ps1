96cc6fa2b0e1f7976d9deaeebc2a50adc1cea67e
Devyn Spencer <devynspencer@users.noreply.github.com>
2020-12-02 12:49:05 -0800

    Splat many parameters

--- Public/New-ZohoAccessToken.ps1
+++ Public/New-ZohoAccessToken.ps1
@@ -18,5 +18,10 @@ function New-ZohoAccessToken {
         $Uri = "$Uri&redirect_uri=$RedirectUri"
     }
 
-    Invoke-RestMethod -Method Post -Uri $Uri
+    $RestMethodParameters = @{
+        Uri = $Uri
+        Method = "Post"
+    }
+
+    Invoke-RestMethod @RestMethodParameters
 }
