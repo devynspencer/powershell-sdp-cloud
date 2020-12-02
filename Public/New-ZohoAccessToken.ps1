65179d795f90745856e30c8d6d86e7704cefc25c
Devyn Spencer <devynspencer@users.noreply.github.com>
2020-12-02 12:57:06 -0800

    Replace query string with body parameters

--- Public/New-ZohoAccessToken.ps1
+++ Public/New-ZohoAccessToken.ps1
@@ -12,15 +12,23 @@ function New-ZohoAccessToken {
         $RedirectUri
     )
 
-    $Uri = "https://accounts.zoho.com/oauth/v2/token?code=$GrantToken&grant_type=authorization_code&client_id=$ClientId&client_secret=$ClientSecret"
+    $Uri = "https://accounts.zoho.com/oauth/v2/token"
+
+    $Body = @{
+        code = $GrantToken
+        grant_type = "authorization_code"
+        client_id = $ClientId
+        client_secret = $ClientSecret
+    }
 
     if ($RedirectUri) {
-        $Uri = "$Uri&redirect_uri=$RedirectUri"
+        $Body.redirect_uri = $RedirectUri
     }
 
     $RestMethodParameters = @{
         Uri = $Uri
         Method = "Post"
+        Body = $Body
     }
 
     Invoke-RestMethod @RestMethodParameters
