11b0a0a4ed187bb3e36fd2e3a7913002b2de2849
Devyn Spencer <devynspencer@users.noreply.github.com>
2020-12-04 19:33:17 -0800

    Reuse existing functions

--- Public/Resolve-ServiceDeskRequest.ps1
+++ Public/Resolve-ServiceDeskRequest.ps1
@@ -1,3 +1,5 @@
+. "$PSScriptRoot\Set-ServiceDeskRequest"
+
 function Resolve-ServiceDeskRequest {
     param (
         [Parameter(Mandatory)]
@@ -13,49 +15,5 @@ function Resolve-ServiceDeskRequest {
         $Message = "Work complete, resolving ticket. Respond to reopen ticket."
     )
 
-    $Headers = @{
-        Authorization = "Zoho-oauthtoken $AccessToken"
-        Accept = "application/vnd.manageengine.sdp.v3+json"
-        "Content-Type" = "application/x-www-form-urlencoded"
-    }
-
-    $Data = @{
-        request = @{
-            resolution = @{
-                content = $Message
-            }
-
-            status = @{
-                name = "Resolved"
-            }
-        }
-    }
-
-    $Body = @{
-        input_data = ($Data | ConvertTo-Json -Depth 4 -Compress)
-    }
-
-    $RestMethodParameters = @{
-        Uri = "https://sdpondemand.manageengine.com/app/$Portal/api/v3/requests/$Id"
-        Headers = $Headers
-        Method = "Put"
-        Body = $Body
-    }
-
-    $Response = (Invoke-RestMethod @RestMethodParameters).request
-
-    $Request = [ordered] @{
-        CompletedTime = $Response.completed_time.display_value
-        DisplayId = $Response.display_id
-        DueTime = $Response.due_by_time.display_value
-        Id = $Response.id
-        Requester = $Response.requester.email_id
-        Resolution = $Response.resolution
-        ResolvedTime = $Response.resolved_time.display_value
-        Status = $Response.status.name
-        Subject = $Response.subject
-        Technician = $Response.technician.email_id
-    }
-
-    [pscustomobject] $Request
+    Set-ServiceDeskRequest -AccessToken $AccessToken -Portal $Portal -Id $Id -Resolution $Message -Status Resolved
 }
