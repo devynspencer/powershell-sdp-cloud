dbe566b5f511d34213a1f9a6247ced45b3507ae6
Devyn Spencer <devynspencer@users.noreply.github.com>
2020-12-04 20:57:24 -0800

    Add Suspend-ServiceDeskRequest
    Resolves #6

---
+++ Public/Suspend-ServiceDeskRequest.ps1
@@ -0,0 +1,63 @@
+. "$PSScriptRoot\..\Private\ConvertTo-UnixMillisecond.ps1"
+
+function Suspend-ServiceDeskRequest {
+    param (
+        [Parameter(Mandatory)]
+        $AccessToken,
+
+        [Parameter(Mandatory)]
+        $Portal,
+
+        [Parameter(Mandatory)]
+        $Id,
+
+        $Until,
+
+        $Message
+    )
+
+    $Headers = @{
+        Authorization = "Zoho-oauthtoken $AccessToken"
+        Accept = "application/vnd.manageengine.sdp.v3+json"
+        "Content-Type" = "application/x-www-form-urlencoded"
+    }
+
+    $Data = @{
+        request = @{
+            status = @{
+                name = "Onhold"
+            }
+        }
+    }
+
+    if ($Until) {
+        $Data.request.onhold_scheduler = @{
+            change_to_status = @{
+                name = "Open"
+            }
+
+            scheduled_time = @{
+                value = ConvertTo-UnixMillisecond $Until
+            }
+        }
+
+        if ($Message) {
+            $Data.request.onhold_scheduler.comments = $Message
+        }
+    }
+
+    $Body = @{
+        input_data = ($Data | ConvertTo-Json -Depth 5 -Compress)
+    }
+
+    Write-Host -ForegroundColor Cyan $Body.input_data
+
+    $RestMethodParameters = @{
+        Uri = "https://sdpondemand.manageengine.com/app/$Portal/api/v3/requests/$Id"
+        Headers = $Headers
+        Method = "Put"
+        Body = $Body
+    }
+
+    $Response = (Invoke-RestMethod @RestMethodParameters).request
+}
