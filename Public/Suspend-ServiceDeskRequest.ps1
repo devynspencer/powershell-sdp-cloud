d63424b98d966e787664b663f036edb6dc26f7b9
Devyn Spencer <devynspencer@users.noreply.github.com>
2020-12-05 21:25:57 -0800

    Accept pipeline input
    Multiple objects on the pipeline need to be iterated over in order to support pipeline input, obviously.
    
    It would appear this was forgotten on the previous commits for pipeline input.

--- Public/Suspend-ServiceDeskRequest.ps1
+++ Public/Suspend-ServiceDeskRequest.ps1
@@ -16,46 +16,52 @@ function Suspend-ServiceDeskRequest {
         $Message
     )
 
-    $Headers = @{
-        Authorization = "Zoho-oauthtoken $AccessToken"
-        Accept = "application/vnd.manageengine.sdp.v3+json"
-        "Content-Type" = "application/x-www-form-urlencoded"
-    }
-
-    $Data = @{
-        request = @{
-            status = @{
-                name = "Onhold"
-            }
-        }
-    }
-
-    if ($Until) {
-        $Data.request.onhold_scheduler = @{
-            change_to_status = @{
-                name = "Open"
-            }
-
-            scheduled_time = @{
-                value = ConvertTo-UnixMillisecond $Until
-            }
+    begin {
+        $Headers = @{
+            Authorization = "Zoho-oauthtoken $AccessToken"
+            Accept = "application/vnd.manageengine.sdp.v3+json"
+            "Content-Type" = "application/x-www-form-urlencoded"
         }
 
-        if ($Message) {
-            $Data.request.onhold_scheduler.comments = $Message
+        $Data = @{
+            request = @{
+                status = @{
+                    name = "Onhold"
+                }
+            }
         }
+    
+        if ($Until) {
+            $Data.request.onhold_scheduler = @{
+                change_to_status = @{
+                    name = "Open"
+                }
+    
+                scheduled_time = @{
+                    value = ConvertTo-UnixMillisecond $Until
+                }
+            }
+    
+            if ($Message) {
+                $Data.request.onhold_scheduler.comments = $Message
+            }
+        }
+    
+        $Body = @{
+            input_data = ($Data | ConvertTo-Json -Depth 5 -Compress)
+        }    
     }
 
-    $Body = @{
-        input_data = ($Data | ConvertTo-Json -Depth 5 -Compress)
+    process {
+        foreach ($RequestId in $Id) {
+            $RestMethodParameters = @{
+                Uri = "https://sdpondemand.manageengine.com/app/$Portal/api/v3/requests/$RequestId"
+                Headers = $Headers
+                Method = "Put"
+                Body = $Body
+            }
+        
+            $Response = (Invoke-RestMethod @RestMethodParameters).request
+        }    
     }
-
-    $RestMethodParameters = @{
-        Uri = "https://sdpondemand.manageengine.com/app/$Portal/api/v3/requests/$Id"
-        Headers = $Headers
-        Method = "Put"
-        Body = $Body
-    }
-
-    $Response = (Invoke-RestMethod @RestMethodParameters).request
 }
