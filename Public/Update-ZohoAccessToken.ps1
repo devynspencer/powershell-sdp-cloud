f15e78b9c53f3856f151b3f6b71693a99f71cddf
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-06-28 13:43:24 -0700

    Support access token refresh using a JSON file

--- Public/Update-ZohoAccessToken.ps1
+++ Public/Update-ZohoAccessToken.ps1
@@ -1,24 +1,38 @@
 function Update-ZohoAccessToken {
+    [CmdletBinding(DefaultParameterSetName="FromFile")]
     param (
         [Parameter(Mandatory)]
         $RefreshToken,
 
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ParameterSetName="FromParams")]
         $ClientId,
 
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ParameterSetName="FromParams")]
         $ClientSecret,
 
-        [Parameter(Mandatory)]
-        $Scope
+        [Parameter(Mandatory, ParameterSetName="FromParams")]
+        $Scope,
+
+        [Parameter(Mandatory, ParameterSetName="FromFile")]
+        $FilePath
     )
 
     $Body = @{
         refresh_token = $RefreshToken
         grant_type = "refresh_token"
-        client_id = $ClientId
-        client_secret = $ClientSecret
-        scope = $Scope
+    }
+
+    if ($PSBoundParameters.ContainsKey("FilePath")) {
+        $FileContent = Get-Content -Raw -Path $FilePath | ConvertFrom-Json
+        $Body.client_id = $FileContent.client_id
+        $Body.client_secret = $FileContent.client_secret
+        $Body.scope = $FileContent.scope -join ','
+    }
+
+    else {
+        $Body.client_id = $ClientId
+        $Body.client_secret = $ClientSecret
+        $Body.scope = $Scope
     }
 
     $RestMethodParameters = @{
