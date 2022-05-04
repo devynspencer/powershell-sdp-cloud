8da5eb38f86db6c551f5881ff4874076f697774b
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-05-04 14:35:19 -0700

    feat: add user params to query format function

--- ServiceDeskPlusCloud/Private/Format-ZohoSearch.ps1
+++ ServiceDeskPlusCloud/Private/Format-ZohoSearch.ps1
@@ -4,13 +4,55 @@
 
 function Format-ZohoSearch {
     param (
-        $Status
+        $Status,
+
+        [string]
+        $Technician,
+
+        [Alias('CreatedBy')]
+        [string]
+        $Creator,
+
+        [string]
+        $Requester
     )
 
     $Criteria = @()
 
     if ($PSBoundParameters.ContainsKey('Status')) {
         $Criteria += Format-ZohoCriteria -Field 'status.name' -Condition 'is' -Values $Status
+    }
+
+    # Handle "user" field inputs, resolve names and email addresses to appropriate field
+    $UserParams = @{
+        Condition = 'is'
+    }
+
+    if ($PSBoundParameters.ContainsKey('Technician')) {
+        $TechParams = @{
+            Values = $Technician
+            Field = Resolve-UserField $Technician -Field 'technician'
+        }
+
+        $Criteria += Format-ZohoCriteria @TechParams
+    }
+
+    if ($PSBoundParameters.ContainsKey('Creator')) {
+        $CreatorParams = @{
+            Values = $Creator
+            Field = Resolve-UserField $Creator -Field 'creator'
+        }
+
+        $Criteria += Format-ZohoCriteria @CreatorParams
+    }
+
+    if ($PSBoundParameters.ContainsKey('Requester')) {
+        $RequesterParams = @{
+            Values = $Requester
+            Field = Resolve-UserField $Requester -Field 'requester'
+        }
+
+        $Criteria += Format-ZohoCriteria @RequesterParams
     }
 
     $SearchObj = @{
