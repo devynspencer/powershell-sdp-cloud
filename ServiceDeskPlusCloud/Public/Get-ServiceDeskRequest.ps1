45d67c98fbfc736c44dc02fe500a692cf21d0aa6
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-09-10 17:30:37 -0700

    Update comment-based help

--- ServiceDeskPlusCloud/Public/Get-ServiceDeskRequest.ps1
+++ ServiceDeskPlusCloud/Public/Get-ServiceDeskRequest.ps1
@@ -1,5 +1,20 @@
 . "$PSScriptRoot\..\private\Format-ZohoHeader.ps1"
 
+<#
+.SYNOPSIS
+    Get a ServiceDesk Plus request by id.
+
+.PARAMETER Portal
+    The portal for the ServiceDesk Plus Cloud instance.
+
+.PARAMETER Id
+    The id of the ServiceDesk Plus request.
+
+.EXAMPLE
+    Get-ServiceDeskRequest -Portal portalname -Id 123456
+    Get request with id 123456.
+#>
+
 function Get-ServiceDeskRequest {
     param (
         [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
