90dd747059232f433684b9eb72d877110783c94a
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-09-10 17:31:47 -0700

    Update comment-based help

--- ServiceDeskPlusCloud/Public/New-ServiceDeskRequest.ps1
+++ ServiceDeskPlusCloud/Public/New-ServiceDeskRequest.ps1
@@ -1,5 +1,50 @@
 . "$PSScriptRoot\..\private\Format-ZohoHeader.ps1"
 
+<#
+.SYNOPSIS
+    Create a new ServiceDesk Plus request.
+
+.PARAMETER Portal
+    The portal for the ServiceDesk Plus Cloud instance.
+
+.PARAMETER Subject
+    The subject of the request.
+
+.PARAMETER Description
+    The description of the request.
+
+.PARAMETER Requester
+    The requester email of the request.
+
+.PARAMETER Group
+    The group name of the request.
+
+.PARAMETER Technician
+    The technician of the request.
+
+.PARAMETER Category
+    The category name of the request.
+
+.PARAMETER SubCategory
+    The subcategory name of the request.
+
+.PARAMETER SubCategoryItem
+    The subcategory item name of the request.
+
+.PARAMETER Site
+    The site name of the request.
+
+.PARAMETER Priority
+    The priority of the request.
+
+.PARAMETER Impact
+    The impact of the request.
+
+.EXAMPLE
+    New-ServiceDeskRequest -Portal portalname -Subject "Perform action x on y server"
+    Create a new request with a specific subject.
+#>
+
 function New-ServiceDeskRequest {
     param (
         [Parameter(Mandatory)]
