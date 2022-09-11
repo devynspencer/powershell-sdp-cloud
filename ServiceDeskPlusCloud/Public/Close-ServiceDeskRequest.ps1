c77ceceb1b1ab68d110b751107905b403de6a983
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-09-10 17:30:00 -0700

    Update comment-based help

--- ServiceDeskPlusCloud/Public/Close-ServiceDeskRequest.ps1
+++ ServiceDeskPlusCloud/Public/Close-ServiceDeskRequest.ps1
@@ -1,5 +1,23 @@
 . "$PSScriptRoot\Set-ServiceDeskRequest"
 
+<#
+.SYNOPSIS
+    Close a ServiceDesk Plus request.
+
+.PARAMETER Portal
+    The portal for the ServiceDesk Plus Cloud instance.
+
+.PARAMETER Id
+    The id of the ServiceDesk Plus request.
+
+.PARAMETER Message
+    The message to include in the resolution of the request.
+
+.EXAMPLE
+    Close-ServiceDeskRequest -Portal portalname -Id 123456
+    Close request 123456 in the specified ServiceDesk Plus Cloud instance.
+#>
+
 function Close-ServiceDeskRequest {
     param (
         [Parameter(Mandatory)]
