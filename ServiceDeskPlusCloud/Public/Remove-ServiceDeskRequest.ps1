cd5aadb5112f4cd65d00f6d05e0eb2dafb013ad8
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-09-10 17:32:08 -0700

    Update comment-based help

--- ServiceDeskPlusCloud/Public/Remove-ServiceDeskRequest.ps1
+++ ServiceDeskPlusCloud/Public/Remove-ServiceDeskRequest.ps1
@@ -1,5 +1,20 @@
 . "$PSScriptRoot\..\private\Format-ZohoHeader.ps1"
 
+<#
+.SYNOPSIS
+    Remove a ServiceDesk Plus request by id.
+
+.PARAMETER Portal
+    The portal for the ServiceDesk Plus Cloud instance.
+
+.PARAMETER Id
+    The id of the ServiceDesk Plus request.
+
+.EXAMPLE
+    Remove-ServiceDeskRequest -Portal portalname -Id 123456
+    Add a note to request 123456 in the specified ServiceDesk Plus Cloud instance.
+#>
+
 function Remove-ServiceDeskRequest {
     param (
         [Parameter(Mandatory)]
