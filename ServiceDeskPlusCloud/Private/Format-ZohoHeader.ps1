da7c8b4155a0de3e79d0ad6b79e46fa93138b397
devynspencer <devynspencer@users.noreply.github.com>
2023-05-21 13:27:35 -0700

    Add Accept param for HTTP media type

--- ServiceDeskPlusCloud/Private/Format-ZohoHeader.ps1
+++ ServiceDeskPlusCloud/Private/Format-ZohoHeader.ps1
@@ -1,13 +1,15 @@
 function Format-ZohoHeader {
     param (
         # Client access token to authenticate with
-        $AccessToken = (Get-Secret -Vault Zoho -AsPlainText -Name 'ACCESS_TOKEN')
+        $AccessToken = (Get-Secret -Vault Zoho -AsPlainText -Name 'ACCESS_TOKEN'),
+
+        $Accept = 'application/vnd.manageengine.sdp.v3+json'
     )
 
     # Build header object
     $Headers = @{
         Authorization = "Zoho-Oauthtoken $AccessToken"
-        Accept = 'application/vnd.manageengine.sdp.v3+json'
+        Accept = $Accept
     }
 
     $Headers
