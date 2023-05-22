1205104617dd0ed5b682e8db4e124daa50438716
devynspencer <devynspencer@users.noreply.github.com>
2023-05-22 14:11:21 -0700

    Update parameter documentation

--- ServiceDeskPlusCloud/Private/Invoke-ServiceDeskApi.ps1
+++ ServiceDeskPlusCloud/Private/Invoke-ServiceDeskApi.ps1
@@ -14,22 +14,24 @@ function Invoke-ServiceDeskApi {
     # TODO: Would parameter sets based on each operation/group of similar operations be cleaner?
 
     param (
+        # The ServiceDesk Plus cloud portal name for the request.
         [Parameter(Mandatory)]
         $Portal,
 
+        # The API operation to perform. Determines structure of request URI, as well as required parameters.
         [Parameter(Mandatory)]
         [ValidateSet('Get', 'List', 'New', 'Update', 'Remove', 'GetChild', 'ListChild', 'AddChild', 'UpdateChild', 'RemoveChild')]
         $Operation,
 
-        # Resource type to operate on.
+        # The base resource type to operate on. Determines structure of request URI, as well as acceptable child resources (if applicable).
         [Parameter(Mandatory)]
         [ValidateSet('requests', 'problems', 'assets', 'projects', 'changes', 'tasks', 'solutions', 'topics')]
         $Resource,
 
-        # Id of an existing resource to operate on.
+        # The Id of an existing resource to operate on (if applicable). Required for operations on a specific resource, i.e. Get/Update/Remove, as well as any child resource operation.
         $Id,
 
-        # Child resource type, used when adding a child resource to an existing resource.
+        # The Child resource type to operate on (of applicable). Used when adding a child resource to an existing resource. Required for any child resource operation.
         #
         # Example: adding a task to a request
         #   api/v3/requests/{request_id}/tasks
@@ -37,19 +39,23 @@ function Invoke-ServiceDeskApi {
         [ValidateSet('tasks', 'notes', 'worklogs', 'approvals', 'relations')]
         $ChildResource,
 
-        # Id of an existing child resource to operate on.
+        # The id of an existing child resource to operate on (if applicable). Required for operations on specific child resources, i.e. GetChild/UpdateChild/RemoveChild.
         #
         # Example: Edit a task attached to a request
         #   api/v3/requests/{request_id}/tasks/{task_id}
         #
         $ChildId,
 
+        # The ServiceDesk Plus cloud API version to use for the operation. The v3 API is used for most operations, but some resources may require v2.
+        [ValidateSet('v2', 'v3')]
         $ApiVersion = 'v3',
 
+        # The HTTP method to use for the operation, based on the ServiceDesk Plus cloud API documentation.
         [ValidateSet('Get', 'Post', 'Put', 'Patch', 'Delete')]
         $Method = 'Get',
 
         # TODO: Add search criteria
+        # Search criteria to apply to the operation (if applicable).
         # $Search = @{}
 
         # TODO: Add Fields parameter to restrict fields in returned object (using input_data)
