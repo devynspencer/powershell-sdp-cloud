7d0940fb4b2dcf539d3eb14f0bccb9747c23ff77
Devyn Spencer <devynspencer@users.noreply.github.com>
2024-01-31 12:15:26 -0800

    Add ContextType parameter to Format-ZohoHeader

--- ServiceDeskPlusCloud/Private/Format-ZohoHeader.ps1
+++ ServiceDeskPlusCloud/Private/Format-ZohoHeader.ps1
@@ -3,13 +3,16 @@ function Format-ZohoHeader {
         # Client access token to authenticate with
         $AccessToken = (Get-Secret -Vault Zoho -AsPlainText -Name 'ACCESS_TOKEN'),
 
-        $Accept = 'application/vnd.manageengine.sdp.v3+json'
+        $Accept = 'application/vnd.manageengine.sdp.v3+json',
+
+        $ContentType = 'application/x-www-form-urlencoded'
     )
 
     # Build header object
     $Headers = @{
-        Authorization = "Zoho-Oauthtoken $AccessToken"
-        Accept = $Accept
+        'Authorization' = "Zoho-Oauthtoken $AccessToken"
+        'Accept' = $Accept
+        'Content-Type' = $ContentType
     }
 
     Write-Verbose "Constructed headers:`n$(ConvertTo-Json $Headers)"
