2584f5dd4f721caf12d7e8e479b6d8950c898cee
Devyn Spencer <devynspencer@users.noreply.github.com>
2024-06-25 15:40:34 -0700

    Remove unused functions

--- ServiceDeskPlusCloud/Private/ConvertTo-UnixMillisecond.ps1
+++
@@ -1,8 +0,0 @@
-function ConvertTo-UnixMillisecond {
-    param (
-        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-        $Date
-    )
-
-    ([Int64] (Get-Date $Date -UFormat %s)) * 1000
-}
