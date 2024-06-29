24f9e6963f785be73295712d59dd6f3293284c08
Devyn Spencer <devynspencer@users.noreply.github.com>
2024-06-29 14:27:00 -0700

    Update functions to use Invoke-ServiceDeskApi

--- ServiceDeskPlusCloud/Public/Remove-ServiceDeskRequest.ps1
+++ ServiceDeskPlusCloud/Public/Remove-ServiceDeskRequest.ps1
@@ -1,4 +1,4 @@
-. "$PSScriptRoot\..\private\Format-ZohoHeader.ps1"
+. "$PSScriptRoot\Invoke-ServiceDeskApi.ps1"
 
 <#
 .SYNOPSIS
@@ -27,13 +27,16 @@ function Remove-ServiceDeskRequest {
 
     process {
         foreach ($RequestId in $Id) {
-            $RestMethodParameters = @{
-                Uri = "https://sdpondemand.manageengine.com/app/$Portal/api/v3/requests/$RequestId"
-                Headers = Format-ZohoHeader
+            $InvokeParams = @{
                 Method = 'Delete'
+                Operation = 'Remove'
+                Resource = 'requests'
+                Id = $RequestId
             }
 
-            Invoke-RestMethod @RestMethodParameters
+            $Response = Invoke-ServiceDeskApi @InvokeParams
+
+            $Response
         }
     }
 }
